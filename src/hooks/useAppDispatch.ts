d18956ada4f86ab4ed19eb0b9c3980edadcaa250
Alex Sim <al.simir@mail.ru>
2023-05-14 22:07:33 +0500

    Updates

--- src/hooks/useAppDispatch.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch } from 'react-redux';
-import { store } from 'store/store';
-
-export type AppDispatch = typeof store.dispatch;
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
