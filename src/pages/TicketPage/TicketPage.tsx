5d58fe4d59cbe79c620c0d7fd3878214d95ff055
Alex Sim <al.simir@mail.ru>
2023-05-15 00:02:21 +0500

    finally

--- src/pages/TicketPage/TicketPage.tsx
+++ src/pages/TicketPage/TicketPage.tsx
@@ -1,17 +1,21 @@
-import Ticket from 'components/Ticket/Ticket'
-import styles from './style.module.css'
-import { NavLink } from 'react-router-dom'
+import Ticket from "components/Ticket/Ticket";
+import styles from "./style.module.css";
+import { NavLink } from "react-router-dom";
 
 const TicketPage = () => {
   return (
     <div className={styles.wrapper}>
-        <NavLink to="/" className={styles['header-wrapper']}>
-            <img src="/images/arrow-return.svg" alt="return" className={styles.arrow} />
-            <p className={styles.header}>Вернуться к задачам</p>
-        </NavLink>
-        <Ticket />
+      <NavLink to="/" className={styles["header-wrapper"]}>
+        <img
+          src="https://svetleshka.github.io/React-YS/images/arrow-return.svg"
+          alt="return"
+          className={styles.arrow}
+        />
+        <p className={styles.header}>Вернуться к задачам</p>
+      </NavLink>
+      <Ticket />
     </div>
-  )
-}
+  );
+};
 
-export default TicketPage
+export default TicketPage;
